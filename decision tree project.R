3d2236de1f1f578ad654fecfe37e40afee07f510
Muhammad Ehsan <ehsanattari52@gmail.com>
2025-02-26 18:58:08 +0500

    Delete decision tree project.R

--- decision tree project.R
+++
@@ -1,9 +0,0 @@
-library(rpart)
-library(rpart.plot)
-library("readxl")
-customer_dbase<-
-  read_excel("F:/Data Analytics/Data Sources/customer_dbase.xlsx")
-View(customer_dbase)  
-attach(customer_dbase)
-z<-rpart(income~creddebt)
-rpart.plot(z)
